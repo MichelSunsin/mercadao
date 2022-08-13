c6df4cdb7cb0bd05b133f22b7bff3493b8f3ec6b
Michel de Castro Sunsin <mintchel@gmail.com>
2022-08-13 18:27:22 -0300

    Home page initial setup

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,36 @@
+import { useState } from 'react';
+
 import './styles.scss';
 
 function Home() {
-  return <div className="home">Home</div>;
+  const categories = [
+    { id: null, desc: 'Todas' },
+    { id: 0, desc: 'Categoria 1' },
+    { id: 1, desc: 'Categoria 2' },
+    { id: 3, desc: 'Categoria 3' },
+  ];
+
+  const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
+
+  return (
+    <div className="home-container">
+      <div className="header">Mercadão de Garça</div>
+      <div className="sidenav">
+        <input className="mrc-input" placeholder="Buscar produto" />
+        <h3>Categorias</h3>
+        {categories.map((category) => (
+          <button
+            type="button"
+            className={`${selectedCategory === category.id ? 'active' : ''}`}
+            onClick={() => setSelectedCategory(category.id)}
+          >
+            {category.desc}
+          </button>
+        ))}
+      </div>
+      <div className="product-listing">Listagem de produtos</div>
+    </div>
+  );
 }
 
 export default Home;
